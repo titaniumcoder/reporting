2112ed457e1cc61d340c534ee97ea57697638a0b
Rico Metzger <rico.metzger@gmail.com>
2019-08-29 19:48:24 +0200

    missing registry entry

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+echo "In Heroku-Deploy.sh, deploying to heroku with $DOCKER_USERNAME"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin registry.heroku.com
 docker push registry.heroku.com/kotlin-reporting/web
