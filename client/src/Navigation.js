a6a16e9a0474a029229bce2aa9a55d897f080cf7
Rico Metzger <rico.metzger@gmail.com>
2020-01-02 07:59:29 +0100

    fixed issue with navigation

--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Button, Collapse, Nav, Navbar, NavbarText, NavbarToggler, NavItem, NavLink} from "reactstrap";
+import {Button, Collapse, Nav, Navbar, NavbarText, NavbarToggler, NavItem} from "reactstrap";
 import {GoogleLogout} from "react-google-login";
 import {GoogleClientId} from "./constants";
 import {useDispatch, useSelector} from "react-redux";
@@ -7,6 +7,7 @@ import {logout} from "./auth/authSlice";
 import {logout as apiLogout} from './api/reportingApi';
 
 import * as moment from 'moment';
+import {NavLink} from "react-router-dom";
 
 const Navigation = ({admin}) => {
     const [isOpen, setIsOpen] = useState(false);
@@ -34,10 +35,12 @@ const Navigation = ({admin}) => {
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="mr-auto" navbar>
                         <NavItem>
-                            <NavLink href="/">Time</NavLink>
+                            <NavLink to="/" activeClassName="active" exact={true}
+                                     className="nav-link">Time</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="/admin">Administration</NavLink>
+                            <NavLink to="/admin" activeClassName="active"
+                                     className="nav-link">Administration</NavLink>
                         </NavItem>
                     </Nav>
                     <Nav>
@@ -47,7 +50,8 @@ const Navigation = ({admin}) => {
                             onLogoutSuccess={executeLogout}
                             render={renderProps => (
                                 <NavItem>
-                                    <Button color="light" onClick={renderProps.onClick} disabled={renderProps.disabled}>Logout</Button>
+                                    <Button color="light" onClick={renderProps.onClick}
+                                            disabled={renderProps.disabled}>Logout</Button>
                                 </NavItem>
                             )}
                         />
