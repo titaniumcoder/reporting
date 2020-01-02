1e30c2e298d7d1c1740ede2839b27f1b2eed961e
Rico Metzger <rico.metzger@gmail.com>
2020-01-02 08:49:13 +0100

    Minor fix

--- server/src/main/kotlin/io/github/titaniumcoder/reporting/config/ReportingConfiguration.kt
+++ server/src/main/kotlin/io/github/titaniumcoder/reporting/config/ReportingConfiguration.kt
@@ -7,6 +7,5 @@ import org.springframework.boot.context.properties.ConstructorBinding
 @ConfigurationProperties(prefix = "reporting")
 data class ReportingConfiguration(
         val clientId: String,
-        val clientSecret: String,
         val adminEmail: String
 )
