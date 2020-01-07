03095fc7b26fe714b43e3a7750086262216d8167
Rico Metzger <rico.metzger@gmail.com>
2020-01-07 17:42:35 +0100

    Created necessary view

---
+++ server/src/main/resources/db/migration/V3__overview_view.sql
@@ -0,0 +1,22 @@
+drop view if exists client_overview;
+
+create view client_overview
+as
+select c.id                     as client_id,
+       c.name                   as client_name,
+       c.rate_in_cents_per_hour as client_rate_in_cents_per_hour,
+       c.max_minutes            as client_max_minutes,
+       p.id                     as project_id,
+       p.name                   as project_name,
+       p.rate_in_cents_per_hour as project_rate_in_cents_per_hour,
+       p.max_minutes            as project_max_minutes,
+       (select EXTRACT(EPOCH FROM sum(t.ending - t.starting)) / 60
+        from time_entry t
+        where t.project_id = p.id
+          and billed = true)    as project_billed_minutes,
+       (select EXTRACT(EPOCH FROM sum(t.ending - t.starting)) / 60
+        from time_entry t
+        where t.project_id = p.id
+          and billed = false)   as project_open_minutes
+from client c
+         left join project p on c.id = p.client_id;
