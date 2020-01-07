2841aea0e24cd35ccee5a9c596ef16d7ccfaed4f
Rico Metzger <rico.metzger@gmail.com>
2020-01-07 05:33:11 +0100

    Just adapting the application.yml

--- server/src/main/kotlin/io/github/titaniumcoder/reporting/config/R2dbcConfiguration.kt
+++ server/src/main/kotlin/io/github/titaniumcoder/reporting/config/R2dbcConfiguration.kt
@@ -1,11 +1,39 @@
 package io.github.titaniumcoder.reporting.config
 
-import org.springframework.context.annotation.Configuration
+import io.github.titaniumcoder.reporting.config.WithJdbcSystemProperty.Companion.JDBC_DATABASE_URL_ENV
+import io.r2dbc.spi.ConnectionFactory
+import org.springframework.boot.autoconfigure.r2dbc.ConnectionFactoryBuilder
+import org.springframework.boot.autoconfigure.r2dbc.ConnectionFactoryOptionsBuilderCustomizer
+import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties
+import org.springframework.context.annotation.*
+import org.springframework.core.type.AnnotatedTypeMetadata
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @Configuration
 @EnableR2dbcRepositories
 @EnableTransactionManagement
-class R2dbcConfiguration /* : AbstractR2dbcConfiguration() */ {
+class R2dbcConfiguration {
+    // TODO FIX this
+    @Bean
+    @Conditional(WithJdbcSystemProperty::class)
+    fun connectionFactory(properties: R2dbcProperties,
+                          customizers: List<ConnectionFactoryOptionsBuilderCustomizer>): ConnectionFactory? {
+        properties.url ="r2dbc" + System.getenv(JDBC_DATABASE_URL_ENV)
+
+        return ConnectionFactoryBuilder
+                .create(properties)
+                .customize(customizers)
+                .build()
+    }
+}
+
+class WithJdbcSystemProperty : Condition {
+    override fun matches(context: ConditionContext, metadata: AnnotatedTypeMetadata): Boolean {
+        return System.getenv().containsKey(JDBC_DATABASE_URL_ENV)
+    }
+
+    companion object {
+        val JDBC_DATABASE_URL_ENV = "JDBC_DATABASE_URL"
+    }
 }
