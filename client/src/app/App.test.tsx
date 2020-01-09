48ade58b7125696b5db21a3b62e617f5194be395
Rico Metzger <rico.metzger@gmail.com>
2020-01-09 18:17:46 +0100

    Typo

--- client/src/app/App.test.tsx
+++ client/src/app/App.test.tsx
@@ -3,7 +3,7 @@ import {render} from '@testing-library/react';
 import App from './App';
 import {Provider} from "react-redux";
 import configureStore from 'redux-mock-store';
-import {MemoryRouterq} from "react-router-dom";
+import {MemoryRouter} from "react-router-dom";
 
 const mockStore = configureStore([]);
 
